3b8925afe34782f280c80e55a840cc45b7a631a2
pochacco0417 <itzy1055900172@gmail.com>
2025-09-26 22:05:46 +0200

    Update LFSR_6bit_tb.sv

--- q2/LFSR_6bit_tb.sv
+++ q2/LFSR_6bit_tb.sv
@@ -1,5 +1,32 @@
 module LFSR_6bit_tb;
 
-// complete here
+  logic clk, rst_n, sel;
+  logic [5:0] parallel_in;
+  logic [5:0] parallel_out;
+
+  LFSR_6bit DUT(
+    .clk(clk),
+    .rst_n(rst_n),
+    .sel(sel),
+    .parallel_in(parallel_in),
+    .parallel_out(parallel_out)
+  );
+
+  initial begin
+    clk=0;
+    always #5 clk=~clk;
+  end
+
+  initial begin
+    rst_n=0; sel=0; parallel_in='0;
+    #12 rst_n=1;
+    #10 sel=0; parallel_in=6'b100000;
+    #10 sel=1;
+    #10 $stop;
+  end
+
+  initial begin
+    $monitor("Time=%0t Output=%b", $time, parallel_out);
+  end
 
 endmodule
