0572c034df83ce57c879d9ea55cda695a9e4701e
pochacco0417 <itzy1055900172@gmail.com>
2025-09-26 21:29:25 +0200

    Update LFSR_6bit.sv

--- q2/LFSR_6bit.sv
+++ q2/LFSR_6bit.sv
@@ -4,7 +4,21 @@ module LFSR_6bit (
   input  logic [5:0] parallel_in,
   output logic [5:0] parallel_out
 );
-  // …
-  // Add your description here
-  // …
+  logic [5:0] next_state;
+  always_ff @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+      parallel_out<='0;
+    end else if(!sel) begin
+      parallel_out<=parallel_in;
+    end else begin
+      next_state[0]=parallel_out[5];
+      next_state[1]=parallel_out[0]^parallel_out[5];
+      next_state[2]=parallel_out[1];
+      next_state[3]=parallel_out[2]^parallel_out[5];
+      next_state[4]=parallel_out[3];
+      next_state[5]=parallel_out[4];
+      parallel_out<=next_state;
+    end
+  end
 endmodule
+
