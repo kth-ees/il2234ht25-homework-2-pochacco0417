fdb1721be6ddf74d8eadfe24c0a51950f35a9eb0
pochacco0417 <itzy1055900172@gmail.com>
2025-09-26 10:25:43 +0200

    Update shift_regiter.sv

--- q1/shift_regiter.sv
+++ q1/shift_regiter.sv
@@ -8,6 +8,21 @@ module shift_register #(parameter N=4)
                        output logic [N-1:0] parallel_out,
                        output logic serial_out);
 
-//complete here
+  always_ff @(posedge clk or negedge rst_n) begin
+    if(!rst_n)
+      parallel_out<='0;
+    else if (load_enable) begin
+      if(serial_parallel)begin
+        parallel_out<=parallel_in;
+      end else begin
+        parallel_out<={serial_in, parallel_out[N-1:1]};
+      end
+    end else begin
+      parallel_out<=parallel_out;
+    end
+  end
+
+  assign serail_out<=parallel_out[0];
 
 endmodule
+
