be9d9371b69b3cf68f5edc8f1e9b4da9e6942205
pochacco0417 <itzy1055900172@gmail.com>
2025-09-28 23:11:43 +0200

    Update up_down_counter.sv

--- q3/up_down_counter.sv
+++ q3/up_down_counter.sv
@@ -7,6 +7,32 @@ module up_down_counter #(parameter N = 4)
                         output logic [N-1:0] count_out,
                         output logic carry_out);
   
-  // complete here
+  always_ff @(posedge clk or negedge rst_n) begin
+    if(!rst_n)begin
+      count_out<='0;
+      carrry_out<=1'b0;
+    end else if(load)begin
+      count_out<=input_load;
+      carry_out<=1'b0;
+    end else begin
+      if(up_down)begin
+        if(&count_out)begin
+          carry_out<=1'b1;
+          count_out<='0;
+        end else begin
+          count_out<=count_out+1;
+          carry_out<=1'b0;
+        end
+      end else begin
+        if(count_out=='0)begin
+          carry_out<=1'b1;
+          count_out<={N{1'b1}};
+        end else begin
+          count_out<=count_out-1;
+          carry_out<=1'b0;
+        end
+      end
+    end
+  end
 
 endmodule
